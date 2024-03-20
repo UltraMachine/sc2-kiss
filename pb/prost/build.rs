0bb5c3d4a169788da65ec0c3650d7d6cd485cda0
Armageddon <51183266+UltraMachine@users.noreply.github.com>
2024-03-20 21:34:37 +1000

    add util to generate ids from sc2 data, make feature to serialize `ResponseData`

--- pb/prost/build.rs
+++ pb/prost/build.rs
@@ -7,7 +7,26 @@ fn main() {
 	let mut proto_file = protos_dir.to_owned();
 	proto_file.push("sc2api.proto");
 
+	let serde = "#[cfg_attr(feature = \"serde\", derive(serde::Serialize, serde::Deserialize))]";
+	let serde_with = |path| format!("#[cfg_attr(feature = \"serde\", serde(with = \"{path}\"))]");
 	prost_build::Config::new()
+		.message_attribute("ResponseData", serde)
+		.message_attribute("AbilityData", serde)
+		.enum_attribute("AbilityData.Target", serde)
+		.field_attribute("AbilityData.target", serde_with("srd::target"))
+		.message_attribute("UnitTypeData", serde)
+		.enum_attribute("Race", serde)
+		.field_attribute("UnitTypeData.race", serde_with("srd::race"))
+		.enum_attribute("Attribute", serde)
+		.field_attribute("UnitTypeData.attributes", serde_with("srd::attributes"))
+		.message_attribute("Weapon", serde)
+		.enum_attribute("Weapon.TargetType", serde)
+		.field_attribute("Weapon.type", serde_with("srd::weapon_target"))
+		.message_attribute("DamageBonus", serde)
+		.field_attribute("DamageBonus.attribute", serde_with("srd::attribute"))
+		.message_attribute("UpgradeData", serde)
+		.message_attribute("BuffData", serde)
+		.message_attribute("EffectData", serde)
 		.compile_protos(&[proto_file], &[protos_dir.parent().unwrap()])
 		.unwrap_or_else(|e| panic!("{e}"))
 }
--- pb/prost/src/lib.rs
+++ pb/prost/src/lib.rs
@@ -1,1 +1,41 @@
+#[cfg(feature = "serde")]
+mod srd {
+	use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+	macro_rules! serde_enums {
+		($($mod:ident => $enum:ty),+ $(,)?) => {$(
+			pub mod $mod {
+				use super::*;
+				pub fn serialize<S: Serializer>(num: &i32, s: S) -> Result<S::Ok, S::Error> {
+					<$enum>::try_from(*num)
+						.unwrap_or_default()
+						.serialize(s)
+				}
+				pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<i32, D::Error> {
+					<$enum>::deserialize(d).map(|res| res as i32)
+				}
+			}
+		)+};
+	}
+	serde_enums! {
+		target => crate::ability_data::Target,
+		race => crate::Race,
+		attribute => crate::Attribute,
+		weapon_target => crate::weapon::TargetType,
+	}
+	pub mod attributes {
+		use super::*;
+		pub fn serialize<S: Serializer>(nums: &[i32], s: S) -> Result<S::Ok, S::Error> {
+			nums.iter()
+				.map(|num| crate::Attribute::try_from(*num).unwrap_or_default())
+				.collect::<Vec<_>>()
+				.serialize(s)
+		}
+		pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<i32>, D::Error> {
+			Vec::<crate::Attribute>::deserialize(d)
+				.map(|res| res.into_iter().map(|item| item as i32).collect())
+		}
+	}
+}
+
 include!(concat!(env!("OUT_DIR"), "/sc2api_protocol.rs"));
