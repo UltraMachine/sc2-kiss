a3b6920534a297feff0036e311d6700f45f70a75
Armageddon <51183266+UltraMachine@users.noreply.github.com>
2024-04-02 19:57:40 +1000

    update proto, add optional glam dependency and conversion between points and vectors

--- pb/prost/src/lib.rs
+++ pb/prost/src/lib.rs
@@ -1,5 +1,8 @@
+include!(concat!(env!("OUT_DIR"), "/sc2api_protocol.rs"));
+
 #[cfg(feature = "serde")]
 mod srd {
+	use super::*;
 	use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 	macro_rules! serde_enums {
@@ -18,24 +21,46 @@ mod srd {
 		)+};
 	}
 	serde_enums! {
-		target => crate::ability_data::Target,
-		race => crate::Race,
-		attribute => crate::Attribute,
-		weapon_target => crate::weapon::TargetType,
+		target => ability_data::Target,
+		race => Race,
+		attribute => Attribute,
+		weapon_target => weapon::TargetType,
 	}
 	pub mod attributes {
 		use super::*;
 		pub fn serialize<S: Serializer>(nums: &[i32], s: S) -> Result<S::Ok, S::Error> {
 			nums.iter()
-				.map(|num| crate::Attribute::try_from(*num).unwrap_or_default())
+				.map(|num| Attribute::try_from(*num).unwrap_or_default())
 				.collect::<Vec<_>>()
 				.serialize(s)
 		}
 		pub fn deserialize<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<i32>, D::Error> {
-			Vec::<crate::Attribute>::deserialize(d)
+			Vec::<Attribute>::deserialize(d)
 				.map(|res| res.into_iter().map(|item| item as i32).collect())
 		}
 	}
 }
 
-include!(concat!(env!("OUT_DIR"), "/sc2api_protocol.rs"));
+#[cfg(feature = "glam")]
+mod glam {
+	use crate::{Point, Point2D};
+	use glam::{Vec2, Vec3};
+
+	macro_rules! from {
+		(@ $T:ident[$($f:ident),+] = $F:ident) => {
+			impl From<$F> for $T {
+				fn from(f: $F) -> $T {
+					$T { $($f: f.$f),+ }
+				}
+			}
+		};
+		($($T:ident[$($f:ident),+] = $F:ident)+) => {$(
+			from!(@ $T[$($f),+] = $F);
+			from!(@ $F[$($f),+] = $T);
+		)+};
+	}
+	from! {
+		Vec2[x,y] = Point2D
+		Vec3[x,y,z] = Point
+	}
+}
