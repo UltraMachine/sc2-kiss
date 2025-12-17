3d0f04df9d7f1de912b3e252d25e614b063c8871
Armageddon <51183266+UltraMachine@users.noreply.github.com>
2025-12-17 19:50:01 +1000

    fix chat macro

--- kiss/src/action/chat.rs
+++ kiss/src/action/chat.rs
@@ -25,7 +25,7 @@ impl From<ChatMessage> for Action {
 #[macro_export]
 macro_rules! chat {
 	($($arg:tt)*) => {
-		$crate::chat::ChatMessage {
+		$crate::action::chat::ChatMessage {
 			message: format!($($arg)*),
 			private: false
 		}
@@ -34,7 +34,7 @@ macro_rules! chat {
 #[macro_export]
 macro_rules! chat_team {
 	($($arg:tt)*) => {
-		$crate::chat::ChatMessage {
+		$crate::action::chat::ChatMessage {
 			message: format!($($arg)*),
 			private: true
 		}
